f4cab334fe75335268e6e25d3e0d0ba986edebd2
Chaemin-Lim (임채민) <antraxmin@naver.com>
2025-02-11 18:40:01 +0900

    chore: Spring Boot 프로젝트 세팅

---
+++ src/main/java/com/example/csdaily/CsdailyApplication.java
@@ -0,0 +1,13 @@
+package com.example.csdaily;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CsdailyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CsdailyApplication.class, args);
+	}
+
+}
