45cba613ecbc8e507cccc4dea40ac8655275925c
10cheon00 <10cheon00@gmail.com>
2025-02-21 14:26:19 +0900

    fix: UserQuizChoice 엔티티 추가

---
+++ src/main/java/com/example/csdaily/quiz/entity/UserQuizChoice.java
@@ -0,0 +1,31 @@
+package com.example.csdaily.quiz.entity;
+
+import com.example.csdaily.user.entity.User;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Entity
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserQuizChoice {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @JoinColumn
+    @ManyToOne
+    private User user;
+
+    @JoinColumn
+    @ManyToOne
+    private Quiz quiz;
+
+    @JoinColumn
+    @OneToOne
+    private QuizChoice choice;
+}
