ccf59f328832a64bed941fcbc428835c44a35380
10cheon00 <10cheon00@gmail.com>
2025-02-25 18:36:02 +0900

    feat: cors 설정

--- src/main/java/com/example/csdaily/config/AppConfig.java
+++ src/main/java/com/example/csdaily/config/AppConfig.java
@@ -3,11 +3,22 @@ package com.example.csdaily.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class AppConfig {
+public class AppConfig implements WebMvcConfigurer {
     @Bean
     public RestTemplate restTemplate() {
         return new RestTemplate();
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+            .allowedMethods("*")
+            .allowedOrigins("*")
+            .allowedHeaders("*")
+            .maxAge(3000);
     }
 }
